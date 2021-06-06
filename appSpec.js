c4d25455e7d3ec095a5631683909a85e70805d38
cold89 <79205139+cold89@users.noreply.github.com>
2021-06-07 02:36:12 +0530

    Create appSpec.js
    spec file for checking condition

---
+++ appSpec.js
@@ -0,0 +1,55 @@
+const app = require("app");
+describe(`App: accountBalanceHistory`, () => {
+    const accountBalanceHistory = [
+        {
+            monthNumber: 3, // two months ago
+            account: {
+                balance: { amount: 600 },
+            },
+        },
+        {
+            monthNumber: 0, // current month
+            account: {
+                balance: { amount: 0 },
+            },
+        },
+        {
+            monthNumber: 4, // two months ago
+            account: {
+                balance: { amount: 400 },
+            },
+        },
+        {
+            monthNumber: 2, // two months ago
+            account: {
+                balance: { amount: 200 },
+            },
+        },
+
+        {
+            monthNumber: 1, // last month
+            account: {
+                balance: { amount: 100 },
+            },
+        },
+    ]
+    it(`init Fail: NOT typOf Array `, () => {
+        expect(app.init(11)).toEqual(false);
+    });
+    it(`init Fail: NO request params `, () => {
+        expect(app.init()).toEqual(false);
+    });
+    it(`init Fail: Balnk Array `, () => {
+        expect(app.init({})).toEqual(false);
+    });
+
+    it(`init Success: the balance amount changes by the same amount each month. `, () => {
+        accountBalanceHistory.find((d) => d.monthNumber == 3).account.balance.amount = 300;
+        expect(app.init(accountBalanceHistory)).toEqual(true);
+    });
+
+    it(`init Success: the balance amount changes by the Differnet amount each month. `, () => {
+        accountBalanceHistory.find((d) => d.monthNumber == 3).account.balance.amount = 600;
+        expect(app.init(accountBalanceHistory)).toEqual(false);
+    });
+});
